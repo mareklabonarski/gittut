c0a7e9be13fab819ddb3717209382033bba8ca00
mareklabonarski <mareklabonarski@gmail.com>
2014-12-01 23:07:36 +0100

    path to app added in wsgi script

--- gitut/gitut/wsgi.py
+++ gitut/gitut/wsgi.py
@@ -7,7 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+path = 'C:/Users/Jenkins/Desktop/GIT/app/gitut'
+if path not in sys.path:
+    sys.path.append(path)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gitut.settings")
 
 from django.core.wsgi import get_wsgi_application
