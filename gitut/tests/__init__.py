1a99657b7a54365ad6d317cd8dba2d2bcee223dd
mareklabonarski <mareklabonarski@gmail.com>
2014-12-02 14:38:11 +0100

    db setup cntd

--- gitut/tests/__init__.py
+++ gitut/tests/__init__.py
@@ -1,6 +1,13 @@
 from gitut.settings import DATABASES
+
+import MySQLdb as db
+
 def setup_package():
-    pass
+    con = db.connect(host="localhost")
+    cur = con.cursor()
+    cur.execute('CREATE DATABASE gittut;')
  
 def teardown_package():
-    pass
+    con = db.connect()
+    cur = con.cursor()
+    cur.execute('DROP DATABASE gittut;')
